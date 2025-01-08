d3404c9c94e74822b0ab6dfe048524d51a813ab7
alheekmahlib <haozo89@gmail.com>
2025-01-07 18:09:38 -0800

    version: 0.1.7

--- lib/src/flutter_quran_screen.dart
+++ lib/src/flutter_quran_screen.dart
@@ -221,190 +221,157 @@ class FlutterQuranScreen extends StatelessWidget {
       Function? onAyahPress}) {
     List<String> newSurahs = [];
     return Container(
-        height: deviceSize.height * 0.8,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(
-              flex: 20,
-              child: pageIndex == 0 || pageIndex == 1
+      height: deviceSize.height * 0.8,
+      padding: const EdgeInsets.all(16.0),
+      child: pageIndex == 0 || pageIndex == 1
 
-                  /// This is for first 2 pages of Quran: Al-Fatihah and Al-Baqarah
-                  ? Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SurahHeaderWidget(
-                              ayah: quranCtrl.staticPages[pageIndex].ayahs,
-                              surahNumber ??
-                                  quranCtrl.staticPages[pageIndex].ayahs[0]
-                                      .surahNumber,
-                              isSvg: isSvg,
-                              bannerSvgPath: bannerSvgPath,
-                              bannerSvgWidth: bannerSvgWidth,
-                              bannerSvgHeight: bannerSvgHeight,
-                              surahNameWidth: surahNameWidth,
-                              surahNameHeight: surahNameHeight,
-                              surahNameColor: surahNameColor,
-                              bannerImageHeight: bannerImageHeight,
-                              bannerImageWidth: bannerImageWidth,
-                              bannerImagePath: bannerImagePath,
-                              onSurahBannerLongPress: onSurahBannerLongPress,
-                            ),
-                            if (pageIndex == 1)
-                              BasmallahWidget(
-                                surahNumber: quranCtrl.staticPages[pageIndex]
-                                    .ayahs[0].surahNumber,
-                                basmallahColor: basmallahColor,
-                                basmallahHeight: basmallahHeight,
-                                basmallahWidth: basmallahWidth,
-                              ),
-                            ...quranCtrl.staticPages[pageIndex].lines
-                                .map((line) {
-                              return GetBuilder<BookmarksCtrl>(
-                                builder: (bookmarkCtrl) {
-                                  final bookmarksAyahs = bookmarkCtrl.bookmarks
-                                      .map((bookmark) => bookmark.ayahId)
-                                      .toList();
-                                  return Column(
-                                    children: [
-                                      SizedBox(
-                                          width: deviceSize.width - 32,
-                                          child: QuranLine(
-                                            line,
-                                            bookmarksAyahs,
-                                            bookmarkCtrl.bookmarks,
-                                            boxFit: BoxFit.scaleDown,
-                                            onAyahLongPress: onAyahLongPress,
-                                            bookmarksColor: bookmarksColor,
-                                            textColor: textColor,
-                                            bookmarkList: bookmarkList,
-                                            ayahSelectedBackgroundColor:
-                                                ayahSelectedBackgroundColor,
-                                            onAyahPress: onAyahPress,
-                                          )),
-                                    ],
-                                  );
-                                },
-                              );
-                            }),
-                          ],
-                        ),
+          /// This is for first 2 pages of Quran: Al-Fatihah and Al-Baqarah
+          ? Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SurahHeaderWidget(
+                      ayah: quranCtrl.staticPages[pageIndex].ayahs,
+                      surahNumber ??
+                          quranCtrl.staticPages[pageIndex].ayahs[0].surahNumber,
+                      isSvg: isSvg,
+                      bannerSvgPath: bannerSvgPath,
+                      bannerSvgWidth: bannerSvgWidth,
+                      bannerSvgHeight: bannerSvgHeight,
+                      surahNameWidth: surahNameWidth,
+                      surahNameHeight: surahNameHeight,
+                      surahNameColor: surahNameColor,
+                      bannerImageHeight: bannerImageHeight,
+                      bannerImageWidth: bannerImageWidth,
+                      bannerImagePath: bannerImagePath,
+                      onSurahBannerLongPress: onSurahBannerLongPress,
+                    ),
+                    if (pageIndex == 1)
+                      BasmallahWidget(
+                        surahNumber: quranCtrl
+                            .staticPages[pageIndex].ayahs[0].surahNumber,
+                        basmallahColor: basmallahColor,
+                        basmallahHeight: basmallahHeight,
+                        basmallahWidth: basmallahWidth,
                       ),
-                    )
-
-                  /// Other Quran pages
-                  : LayoutBuilder(builder: (context, constraints) {
-                      return ListView(
-                          physics: currentOrientation == Orientation.portrait
-                              ? const NeverScrollableScrollPhysics()
-                              : null,
-                          children: [
-                            ...quranCtrl.staticPages[pageIndex].lines
-                                .map((line) {
-                              bool firstAyah = false;
-                              if (line.ayahs[0].ayahNumber == 1 &&
-                                  !newSurahs
-                                      .contains(line.ayahs[0].surahNameAr)) {
-                                newSurahs.add(line.ayahs[0].surahNameAr);
-                                firstAyah = true;
-                              }
-                              return GetBuilder<BookmarksCtrl>(
-                                builder: (bookmarkCtrl) {
-                                  final bookmarksAyahs = bookmarkCtrl.bookmarks
-                                      .map((bookmark) => bookmark.ayahId)
-                                      .toList();
-                                  return Column(
-                                    children: [
-                                      if (firstAyah)
-                                        SurahHeaderWidget(
-                                          ayah: quranCtrl
-                                              .staticPages[pageIndex].ayahs,
-                                          surahNumber ??
-                                              line.ayahs[0].surahNumber,
-                                          isSvg: isSvg,
-                                          bannerSvgPath: bannerSvgPath,
-                                          bannerSvgWidth: bannerSvgWidth,
-                                          bannerSvgHeight: bannerSvgHeight,
-                                          surahNameWidth: surahNameWidth,
-                                          surahNameHeight: surahNameHeight,
-                                          surahNameColor: surahNameColor,
-                                          bannerImageHeight: bannerImageHeight,
-                                          bannerImageWidth: bannerImageWidth,
-                                          bannerImagePath: bannerImagePath,
-                                          onSurahBannerLongPress:
-                                              onSurahBannerLongPress,
-                                        ),
-                                      if (firstAyah &&
-                                          (line.ayahs[0].surahNumber != 9))
-                                        BasmallahWidget(
-                                          surahNumber: quranCtrl
-                                              .staticPages[pageIndex]
-                                              .ayahs[0]
-                                              .surahNumber,
-                                          basmallahColor: basmallahColor,
-                                          basmallahHeight: basmallahHeight,
-                                          basmallahWidth: basmallahWidth,
-                                        ),
-                                      SizedBox(
-                                        width: deviceSize.width - 30,
-                                        height: ((currentOrientation ==
-                                                        Orientation.portrait
-                                                    ? constraints.maxHeight
-                                                    : Get.width) -
-                                                (quranCtrl
-                                                        .staticPages[pageIndex]
-                                                        .numberOfNewSurahs *
-                                                    (line.ayahs[0]
-                                                                .surahNumber !=
-                                                            9
-                                                        ? 110
-                                                        : 80))) *
-                                            0.95 /
-                                            quranCtrl.staticPages[pageIndex]
-                                                .lines.length,
-                                        child: QuranLine(
-                                          line,
-                                          bookmarksAyahs,
-                                          bookmarkCtrl.bookmarks,
-                                          boxFit: line.ayahs.last.centered
-                                              ? BoxFit.scaleDown
-                                              : BoxFit.fill,
-                                          onAyahLongPress: onAyahLongPress,
-                                          bookmarksColor: bookmarksColor,
-                                          textColor: textColor,
-                                          bookmarkList: bookmarkList,
-                                          ayahSelectedBackgroundColor:
-                                              ayahSelectedBackgroundColor,
-                                          onAyahPress: onAyahPress,
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            }),
-                          ]);
+                    ...quranCtrl.staticPages[pageIndex].lines.map((line) {
+                      return GetBuilder<BookmarksCtrl>(
+                        builder: (bookmarkCtrl) {
+                          final bookmarksAyahs = bookmarkCtrl.bookmarks
+                              .map((bookmark) => bookmark.ayahId)
+                              .toList();
+                          return Column(
+                            children: [
+                              SizedBox(
+                                  width: deviceSize.width - 32,
+                                  child: QuranLine(
+                                    line,
+                                    bookmarksAyahs,
+                                    bookmarkCtrl.bookmarks,
+                                    boxFit: BoxFit.scaleDown,
+                                    onAyahLongPress: onAyahLongPress,
+                                    bookmarksColor: bookmarksColor,
+                                    textColor: textColor,
+                                    bookmarkList: bookmarkList,
+                                    ayahSelectedBackgroundColor:
+                                        ayahSelectedBackgroundColor,
+                                    onAyahPress: onAyahPress,
+                                  )),
+                            ],
+                          );
+                        },
+                      );
                     }),
-            ),
-            // Expanded(
-            //   flex: 1,
-            //   child: bottomWidget ??
-            //       (showBottomWidget
-            //           ? quranCtrl.staticPages.isEmpty
-            //               ? const CircularProgressIndicator()
-            //               : QuranPageBottomInfoWidget(
-            //                   page: pageIndex + 1,
-            //                   hizb: quranCtrl.staticPages[pageIndex].hizb,
-            //                   surahName: quranCtrl.staticPages[pageIndex].lines
-            //                       .last.ayahs[0].surahNameAr,
-            //                 )
-            //           : const SizedBox.shrink()),
-            // ),
-          ],
-        ));
+                  ],
+                ),
+              ),
+            )
+
+          /// Other Quran pages
+          : LayoutBuilder(builder: (context, constraints) {
+              return ListView(
+                  physics: currentOrientation == Orientation.portrait
+                      ? const NeverScrollableScrollPhysics()
+                      : null,
+                  children: [
+                    ...quranCtrl.staticPages[pageIndex].lines.map((line) {
+                      bool firstAyah = false;
+                      if (line.ayahs[0].ayahNumber == 1 &&
+                          !newSurahs.contains(line.ayahs[0].surahNameAr)) {
+                        newSurahs.add(line.ayahs[0].surahNameAr);
+                        firstAyah = true;
+                      }
+                      return GetBuilder<BookmarksCtrl>(
+                        builder: (bookmarkCtrl) {
+                          final bookmarksAyahs = bookmarkCtrl.bookmarks
+                              .map((bookmark) => bookmark.ayahId)
+                              .toList();
+                          return Column(
+                            children: [
+                              if (firstAyah)
+                                SurahHeaderWidget(
+                                  ayah: quranCtrl.staticPages[pageIndex].ayahs,
+                                  surahNumber ?? line.ayahs[0].surahNumber,
+                                  isSvg: isSvg,
+                                  bannerSvgPath: bannerSvgPath,
+                                  bannerSvgWidth: bannerSvgWidth,
+                                  bannerSvgHeight: bannerSvgHeight,
+                                  surahNameWidth: surahNameWidth,
+                                  surahNameHeight: surahNameHeight,
+                                  surahNameColor: surahNameColor,
+                                  bannerImageHeight: bannerImageHeight,
+                                  bannerImageWidth: bannerImageWidth,
+                                  bannerImagePath: bannerImagePath,
+                                  onSurahBannerLongPress:
+                                      onSurahBannerLongPress,
+                                ),
+                              if (firstAyah && (line.ayahs[0].surahNumber != 9))
+                                BasmallahWidget(
+                                  surahNumber: quranCtrl.staticPages[pageIndex]
+                                      .ayahs[0].surahNumber,
+                                  basmallahColor: basmallahColor,
+                                  basmallahHeight: basmallahHeight,
+                                  basmallahWidth: basmallahWidth,
+                                ),
+                              SizedBox(
+                                width: deviceSize.width - 30,
+                                height:
+                                    ((currentOrientation == Orientation.portrait
+                                                ? constraints.maxHeight
+                                                : Get.width) -
+                                            (quranCtrl.staticPages[pageIndex]
+                                                    .numberOfNewSurahs *
+                                                (line.ayahs[0].surahNumber != 9
+                                                    ? 110
+                                                    : 80))) *
+                                        0.95 /
+                                        quranCtrl.staticPages[pageIndex].lines
+                                            .length,
+                                child: QuranLine(
+                                  line,
+                                  bookmarksAyahs,
+                                  bookmarkCtrl.bookmarks,
+                                  boxFit: line.ayahs.last.centered
+                                      ? BoxFit.scaleDown
+                                      : BoxFit.fill,
+                                  onAyahLongPress: onAyahLongPress,
+                                  bookmarksColor: bookmarksColor,
+                                  textColor: textColor,
+                                  bookmarkList: bookmarkList,
+                                  ayahSelectedBackgroundColor:
+                                      ayahSelectedBackgroundColor,
+                                  onAyahPress: onAyahPress,
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }),
+                  ]);
+            }),
+    );
   }
 }
 
